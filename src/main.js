2510dbb7a79da9ca2a605813059bb2e928505337
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2023-09-16 20:57:21 +0400

    Add performance analytics

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@
 const { app, Menu, Tray, globalShortcut, clipboard } = require('electron');
 const isPendingInstallation = require('electron-squirrel-startup');
 const path = require('path');
+// eslint-disable-next-line import/no-unresolved
+const { initialize, trackEvent } = require('@aptabase/electron/main');
 const { searchAndReplace, applyRegexReplacements } = require('trie-rules');
 const { loadRules } = require('./app');
+
 require('dotenv').config();
+
+initialize(process.env.APTABASE_KEY);
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (isPendingInstallation) {
@@ -17,7 +22,18 @@ if (app.dock) {
 
 const init = async () => {
     const tray = new Tray(path.join(__dirname, 'assets/images/tray.png'));
-    tray.setContextMenu(Menu.buildFromTemplate([{ label: 'Quit', type: 'normal', click: app.quit }]));
+    tray.setContextMenu(
+        Menu.buildFromTemplate([
+            {
+                label: 'Quit',
+                type: 'normal',
+                click: () => {
+                    trackEvent('app_quit');
+                    app.quit();
+                },
+            },
+        ]),
+    );
 
     const { searchReplaceRules, regexRules, totalRulesCount } = await loadRules();
 
@@ -26,14 +42,21 @@ const init = async () => {
     globalShortcut.register('CommandOrControl+Shift+X', () => {
         const text = clipboard.readText();
 
+        const now = performance.now();
         let changed = searchAndReplace(searchReplaceRules, text);
         changed = applyRegexReplacements(regexRules, changed);
 
         if (text !== changed) {
+            const total = performance.now() - now;
             clipboard.writeText(changed);
+            trackEvent('formatted', { length: text.length, total });
         }
     });
+
+    trackEvent('app_started', { logName: process.env.USER || process.env.LOGNAME });
 };
 
-app.on('will-quit', () => globalShortcut.unregisterAll());
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll();
+});
 app.whenReady().then(init);
